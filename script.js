8a584bd89bf260fbeb031acf9d94bddfecce928d
Kevin <kevinjohns207@gmail.com>
2025-06-29 15:06:13 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,144 +3,144 @@ const cards = {
     {
         "front": "SO 1",
         "back": "Keep informed on fire weather conditions and forecasts.",
-        "image": "./SO1.PNG"
+        "image": "./SO1.png"
     },
     {
         "front": "SO 2",
         "back": "Know what your fire is doing at all times.",
-        "image": "./SO2.PNG"
+        "image": "./SO2.png"
     },
     {
         "front": "SO 3",
         "back": "Base all actions on current and expected fire behavior.",
-        "image": "./SO3.PNG"
+        "image": "./SO3.png"
     },
     {
         "front": "SO 4",
         "back": "Identify escape routes and safety zones and make them known.",
-        "image": "./SO4.PNG"
+        "image": "./SO4.png"
     },
     {
         "front": "SO 5",
         "back": "Post lookouts when there is possible danger.",
-        "image": "./SO5.PNG"
+        "image": "./SO5.png"
     },
     {
         "front": "SO 6",
         "back": "Be alert, keep calm, think clearly, act decisively.",
-        "image": "./SO6.PNG"
+        "image": "./SO6.png"
     },
     {
         "front": "SO 7",
         "back": "Maintain prompt communications with your forces, your supervisor, and adjoining forces.",
-        "image": "./SO7.PNG"
+        "image": "./SO7.png"
     },
     {
         "front": "SO 8",
         "back": "Give clear instructions and ensure they are understood.",
-        "image": "./SO8.PNG"
+        "image": "./SO8.png"
     },
     {
         "front": "SO 9",
         "back": "Maintain control of your forces at all times.",
-        "image": "./SO9.PNG"
+        "image": "./SO9.png"
     },
     {
         "front": "SO 10",
         "back": "Fight fire aggressively, having provided for safety first.",
-        "image": "./SO10.PNG"
+        "image": "./SO10.png"
     }
 ],
     "18s": [
     {
         "front": "WO 1",
         "back": "Fire not scouted and sized up.",
-        "image": "./WO1.PNG"
+        "image": "./WO1.png"
     },
     {
         "front": "WO 2",
         "back": "In country not seen in daylight.",
-        "image": "./WO2.PNG"
+        "image": "./WO2.png"
     },
     {
         "front": "WO 3",
         "back": "Safety zones and escape routes not identified.",
-        "image": "./WO3.PNG"
+        "image": "./WO3.png"
     },
     {
         "front": "WO 4",
         "back": "Unfamiliar with weather and local factors influencing fire behavior.",
-        "image": "./WO4.PNG"
+        "image": "./WO4.png"
     },
     {
         "front": "WO 5",
         "back": "Uninformed on strategy, tactics, and hazards.",
-        "image": "./WO5.PNG"
+        "image": "./WO5.png"
     },
     {
         "front": "WO 6",
         "back": "Instructions and assignments not clear.",
-        "image": "./WO6.PNG"
+        "image": "./WO6.png"
     },
     {
         "front": "WO 7",
         "back": "No communication link with crew members or supervisor.",
-        "image": "./WO7.PNG"
+        "image": "./WO7.png"
     },
     {
         "front": "WO 8",
         "back": "Constructing line without safe anchor point.",
-        "image": "./WO8.PNG"
+        "image": "./WO8.png"
     },
     {
         "front": "WO 9",
         "back": "Building fireline downhill with fire below.",
-        "image": "./WO9.PNG"
+        "image": "./WO9.png"
     },
     {
         "front": "WO 10",
         "back": "Attempting frontal assault on fire.",
-        "image": "./WO10.PNG"
+        "image": "./WO10.png"
     },
     {
         "front": "WO 11",
         "back": "Unburned fuel between you and the fire.",
-        "image": "./WO11.PNG"
+        "image": "./WO11.png"
     },
     {
         "front": "WO 12",
         "back": "Cannot see main fire, not in contact with someone who can.",
-        "image": "./WO12.PNG"
+        "image": "./WO12.png"
     },
     {
         "front": "WO 13",
         "back": "On a hillside where rolling material can ignite fuel below.",
-        "image": "./WO13.PNG"
+        "image": "./WO13.png"
     },
     {
         "front": "WO 14",
         "back": "Weather is getting hotter and drier.",
-        "image": "./WO14.PNG"
+        "image": "./WO14.png"
     },
     {
         "front": "WO 15",
         "back": "Wind increases and/or changes direction.",
-        "image": "./WO15.PNG"
+        "image": "./WO15.png"
     },
     {
         "front": "WO 16",
         "back": "Getting frequent spot fires across the line.",
-        "image": "./WO16.PNG"
+        "image": "./WO16.png"
     },
     {
         "front": "WO 17",
         "back": "Terrain and fuels make escape to safety zones difficult.",
-        "image": "./WO17.PNG"
+        "image": "./WO17.png"
     },
     {
         "front": "WO 18",
         "back": "Taking a nap near the fire line.",
-        "image": "./WO18.PNG"
+        "image": "./WO18.png"
     }
 ]
 };
